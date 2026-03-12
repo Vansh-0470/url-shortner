fc96dd6d0bd48e6630ff32016a55834998b75163
Vansh-0470 <vansh89501@gmail.com>
2026-03-12 13:15:54 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# url-shortner
+# iShortener
+URL shortener Using NodeJS, Express & MySQL to generate url shortener & redirect tto original website.
+
+## Features
+- Generate Unique URL for url shortener
+- Redirect to original website from url shortner
+
+## Hashing
+- Using shortid to generate unique has for each url shortener
+
+
+
+
---
+++ app.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-console */
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const validUrl = require('valid-url');
+
+const app = express();
+const port = process.env.PORT || 4040;
+
+const { encrypt, decrypt } = require('./utils/security');
+
+app.use(express.static('public'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+
+app.get('/:hashId', async (req, res) => {
+  const { hashId } = req.params;
+  try {
+    const url = await decrypt(hashId);
+    if (url !== null) {
+      res.redirect(url);
+    } else {
+      throw new Error('Invalid or expired URL');
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.send(err.message);
+  }
+});
+
+app.post('/', async (req, res) => {
+  const { url } = req.body;
+  try {
+    if (validUrl.isUri(url)) {
+      const hash = await encrypt(url);
+      res.json({
+        isError: false,
+        url: `${req.hostname}/${hash}`,
+      });
+    } else {
+      throw new Error('Invalid URL');
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.send(err.message);
+  }
+});
+
+app.get('/', async (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+
+app.listen(port, () => console.log(`Server started on http://localhost:${port}`));
